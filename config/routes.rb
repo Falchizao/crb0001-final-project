573c53287330480c5ee85d970cb1b913ab2e870b
falchizao <marcelonavarro11md@gmail.com>
2023-11-25 15:37:04 -0300

    feat: homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'home/index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :orders do
+    resources :items
+  end
+
+  # root "orders#index" descomentar em prod
+  root "home#index"
 end
